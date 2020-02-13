837f29d467807c98ec61020c8d830071cd0260c8
Robbendebiene <robbendebiene@mailbox.org>
2020-02-13 14:28:12 +0100

    added user script and description

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
+
 # Youtube-Video-Time-Saver
 Simple user script for retaining the video playback time across page reloads and browser restarts. For those who want to pause videos and continue watching them later.
+On the end of the video the start time will be removed so the video will start from the beginning on reload.
+
+## Installation
+[Click here to install]( https://raw.githubusercontent.com/Robbendebiene/Youtube-Video-Time-Saver/master/youtube-video-time-saver.user.js)
+
+## Options
+You can specify a rewind time by changing the **REWIND_TIME** constant.
+
+## FAQ
+**Why is the script injected in the entire youtube website and not just youtube.com/watch?**
+Because youtube doesn't reload the website when you for example click on a video in your subscriptions feed. It just updates the history/url. Therefore the script would only be injected if you reload the video page or visit it directly.
---
+++ youtube-video-time-saver.user.js
@@ -0,0 +1,61 @@
+
+// ==UserScript==
+// @name                One-Click Steam Discovery-Queue
+// @description         Simple user script for automated one-click Steam discovery queue exploring.
+// @version             1.0.0
+// @namespace           https://github.com/Robbendebiene/Youtube-Video-Time-Saver
+// @updateURL           https://raw.githubusercontent.com/Robbendebiene/Youtube-Video-Time-Saver/master/youtube-video-time-saver.user.js
+// @icon                https://www.youtube.com/favicon.ico
+// @match               *://www.youtube.com/*
+// @run-at              document-end
+// ==/UserScript==
+
+// customizable constants
+
+const REWIND_TIME = 0;
+const VIDEO_SELECTOR_STRING = "#movie_player video";
+
+// global variables
+
+let videoPlayer;
+
+main();
+
+window.addEventListener("yt-navigate-finish", main);
+
+function main () {
+  if (window.location.pathname.startsWith("/watch")) {
+    videoPlayer = document.querySelector(VIDEO_SELECTOR_STRING);
+    videoPlayer.addEventListener("timeupdate", update);
+  }
+};
+
+
+function update () {
+  if (window.location.pathname.startsWith("/watch")) {
+    // while playing
+    if (videoPlayer.currentTime < videoPlayer.duration) {
+      const startTime = Math.max(videoPlayer.currentTime - REWIND_TIME, 0);
+      setYTVideoStartTimeParameter(startTime);
+    }
+    // video ended
+    else {
+      clearYTVideoStartTimeParameter();
+    }
+  }
+}
+
+
+function setYTVideoStartTimeParameter (startTime) {
+  startTime = Math.floor(startTime);
+  const currentURL = new URL(window.location.href);
+  currentURL.searchParams.set("t", startTime);
+  window.history.replaceState(null, null, currentURL.href);
+}
+
+
+function clearYTVideoStartTimeParameter () {
+  const currentURL = new URL(window.location.href);
+  currentURL.searchParams.delete("t");
+  window.history.replaceState(null, null, currentURL.href);
+}
