6f35ce8c2ae958622b2b0325e6e1fa3b8342c233
Robbendebiene <robbendebiene@mailbox.org>
2021-04-23 00:37:12 +0200

    changed script to only save video timestamps on video pause and timeline jumps by default

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 
 # Youtube-Video-Time-Saver
 Simple user script for retaining the video playback time across page reloads and browser restarts. For those who want to pause videos and continue watching them later.
+This user script will save the video timestamp in the URL whenever you pause the video or you jump to a different timestamp.
 On the end of the video the start time will be removed so the video will start from the beginning on reload.
 
 ## Installation
@@ -8,6 +9,7 @@ On the end of the video the start time will be removed so the video will start f
 
 ## Options
 You can specify a rewind time by changing the **REWIND_TIME** constant.
+By changing the **SAVE_TIMESTAMP_EVENT** global variable to "timeupdate" the video timestamp will be constantly updated and saved.
 
 ## FAQ
 **Why is the script injected in the entire youtube website and not just youtube.com/watch?**
--- youtube-video-time-saver.user.js
+++ youtube-video-time-saver.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name                Youtube-Video Time Saver
 // @description         Simple user script for retaining the video playback time across page reloads and browser restarts. For those who want to pause videos and continue watching them later.
-// @version             1.0.0
+// @version             1.0.1
 // @namespace           https://github.com/Robbendebiene/Youtube-Video-Time-Saver
 // @updateURL           https://raw.githubusercontent.com/Robbendebiene/Youtube-Video-Time-Saver/master/youtube-video-time-saver.user.js
 // @icon                https://www.youtube.com/favicon.ico
@@ -12,7 +12,11 @@
 
 // customizable constants
 
+// specify an optional rewind time in seconds
 const REWIND_TIME = 0;
+// switch to "timeupdate" if you want to constantly save the video timestamp
+const SAVE_TIMESTAMP_EVENT = "pause";
+// css selector for video player element
 const VIDEO_SELECTOR_STRING = "#movie_player video";
 
 // global variables
@@ -26,22 +30,18 @@ window.addEventListener("yt-navigate-finish", main);
 function main () {
   if (window.location.pathname.startsWith("/watch")) {
     videoPlayer = document.querySelector(VIDEO_SELECTOR_STRING);
-    videoPlayer.addEventListener("timeupdate", update);
+    videoPlayer.addEventListener(SAVE_TIMESTAMP_EVENT, update);
+    // update on timeline changes
+    videoPlayer.addEventListener("seeking", update);
+    videoPlayer.addEventListener("ended", clearYTVideoStartTimeParameter);
   }
 };
 
 
 function update () {
   if (window.location.pathname.startsWith("/watch")) {
-    // while playing
-    if (videoPlayer.currentTime < videoPlayer.duration) {
-      const startTime = Math.max(videoPlayer.currentTime - REWIND_TIME, 0);
-      setYTVideoStartTimeParameter(startTime);
-    }
-    // video ended
-    else {
-      clearYTVideoStartTimeParameter();
-    }
+    const startTime = Math.max(videoPlayer.currentTime - REWIND_TIME, 0);
+    setYTVideoStartTimeParameter(startTime);
   }
 }
 
